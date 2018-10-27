bcdf6182cb6f0c25914607f82429bf93e4be6291
Drew Curtis <drewcurtis99@gmail.com>
2018-10-26 21:53:26 -0600

    Created api endpoints to query 6 random tents or shirts

--- server/index.js
+++ server/index.js
@@ -1,18 +1,30 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-// const mongoose = require('mongoose');
 const { Tent, Shirt, db } = require('../db/index.js');
-// const { tentData, shirtData } = require('../db/seed.js');
 let app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/../client/dist'));
 
+app.get('/data/shirts', (req, res) => {
+  Shirt.aggregate([ { $sample: { size: 6 } } ]).exec((err, data) => {
+    if (err) {
+      console.log('Server Error', err);
+    } else {
+      res.status(200).send(data);
+    }
+  })
+});
 
-  // console.log(db.collections)
-  // db.dropCollection('tents', ()=>{});
-  // db.dropCollection('shirts', ()=>{});
-
+app.get('/data/tents', (req, res) => {
+  Tent.aggregate([ { $sample: { size: 6 } } ]).exec((err, data) => {
+    if (err) {
+      console.log('Server Error', err);
+    } else {
+      res.status(200).send(data);
+    }
+  })
+});
 
 let port = 3000;
 
