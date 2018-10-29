d5fcf7b2b1a8de9ffd536ac91d406a3f54d4a86f
Drew Curtis <drewcurtis99@gmail.com>
2018-10-29 13:08:25 -0600

    created methods to fetch db data, update state, then render on page load

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -3,7 +3,37 @@ import React from 'react';
 export default class App extends React.Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      tents: [],
+      shirts: []
+    };
+
+    this.updateState = this.updateState.bind(this);
+  }
+
+  componentDidMount() {
+    this.getTentData(this.updateState);
+    this.getShirtData(this.updateState);
+  }
+
+  getTentData(cb) {
+    fetch('http://localhost:3004/data/tents')
+    .then(res => res.json())
+    .then(data => cb('tents', data))
+    .catch(error => console.error(error))
+  }
+
+  getShirtData(cb) {
+    fetch('http://localhost:3004/data/shirts')
+    .then(res => res.json())
+    .then(data => cb('shirts', data))
+    .catch(error => console.error(error))
+  }
+
+  updateState(prop, value) {
+    this.setState({
+      [prop]: value
+    })
   }
 
   render () {
